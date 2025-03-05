03b6eb88d187512259cf3581ab198affd09a8d03
R0ct3M <roctem@roctem.com>
2025-03-04 19:07:30 -0500

    Feat, Se crear archivo main.py con menú de opciones

---
+++ main.py
@@ -0,0 +1,18 @@
+nameSheller = None
+
+products = []
+product = {}
+
+ban = 0
+
+while ban != 5:
+    print("------ Menú de Opciones ------")
+    print("1. Nuevo producto")
+    print("2. Eliminar producto")
+    print("3. Realizar factura")
+    print("4. Cancelar factura")
+    print("5. salir")
+
+    print("Digite la opción que desea:")
+    ban = int(input())
+    print("!!Hasta luego, vuelva pronto!!")
